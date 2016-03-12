fe8dfab7fd5b3bee6411b0b2cb3a9bdcd4403617
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-03-12 15:06:03 +0000

    learned nav-controllers

---
+++ nav-controller/OrangeVC.swift
@@ -0,0 +1,40 @@
+//
+//  OrangeVC.swift
+//  nav-controller
+//
+//  Created by Vyacheslav Horbach on 12/03/16.
+//  Copyright © 2016 Vyacheslav Horbach. All rights reserved.
+//
+
+import UIKit
+
+class OrangeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func onPopBtnPressed(sender: AnyObject) {
+        navigationController?.popToRootViewControllerAnimated(true)
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
